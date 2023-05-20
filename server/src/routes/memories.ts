9202e387cae79f369ef5a4caf7a94dfb02b411dc
Fernando D'luccas <dluccash@hotmail.com>
2023-05-20 17:36:16 -0300

    Aula 02

---
+++ server/src/routes/memories.ts
@@ -0,0 +1,98 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+import { z } from 'zod'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get('/memories', async (request, reply) => {
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+
+    return memories.map((memory) => ({
+      id: memory.id,
+      coverUrl: memory.coverUrl,
+      excert: memory.content.substring(0, 115).concat('...'),
+    }))
+  })
+
+  app.get('/memories/:id', async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    return memory
+  })
+
+  app.post('/memories', async (request, reply) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string().url(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: '1',
+      },
+    })
+
+    return memory
+  })
+
+  app.put('/memories/:id', async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string().url(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+    const { id } = paramsSchema.parse(request.params)
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+      },
+    })
+
+    return memory
+  })
+
+  app.delete('/memories/:id', async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+  })
+}
---
+++ web/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react'
+import './globals.css'
+import { Roboto_Flex, Bai_Jamjuree } from 'next/font/google'
+
+const roboto = Roboto_Flex({ subsets: ['latin'], variable: '--font-roboto' })
+const baijamjuree = Bai_Jamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
+
+export const metadata = {
+  title: 'NLW Spacetime',
+  description:
+    'Uma capsúla do tempo construioda com React, Next.js e TailwindCSS',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baijamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
